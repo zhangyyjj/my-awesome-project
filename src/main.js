1bab40988d10151a45a6d3b6144cb8abdc74abf0
zhangyuanjiang <z1404688262@163.com>
2024-04-25 15:39:32 +0800

    fix:图标不显示解决

--- src/main.js
+++ src/main.js
@@ -8,9 +8,14 @@ import ElementPlus from 'element-plus'
 import 'element-plus/es/components/message/style/css'
 import 'element-plus/es/components/message-box/style/css'
 import 'element-plus/theme-chalk/el-loading.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import '@/assets/styles/reset.scss'
 
 const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(store)
 app.use(router)
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1> 测试 vueUse 的鼠标坐标 </h1>
+  <h1>
+    测试 vueUse 的鼠标坐标 <el-icon class="icon"><Promotion /></el-icon>
+  </h1>
   <h3>Mouse: {{ x }} x {{ y }}</h3>
 </template>
 
@@ -9,4 +11,8 @@
   const { x, y } = useMouse()
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .icon {
+    color: aqua;
+  }
+</style>
